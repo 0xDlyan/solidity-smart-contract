51c4dc456e24e05d745f5af714cf9db67a01e2c8
Thierry Dang <thierrydang@gmail.com>
2021-12-22 15:17:32 +0900

    feat-first_compile

--- contracts/DisplayMessage.sol
+++ contracts/DisplayMessage.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract DisplayMessage 
-{
+contract DisplayMessage {
+
 function myMessage() public pure returns (string memory) 
 {
     return "Hello World";
