784e1708eb74a2bf764501a9c41774ab2402bf45
Thierry Dang <thierrydang@gmail.com>
2021-12-22 15:33:30 +0900

    feat-change_text

--- contracts/DisplayMessage.sol
+++ contracts/DisplayMessage.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+// Contract name
 contract DisplayMessage {
 
+// Function that will display our message
 function myMessage() public pure returns (string memory) 
 {
     return "First Smart Contract";
