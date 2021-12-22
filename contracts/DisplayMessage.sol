dd1e43f250868deb5165cde94bc09ca46073b117
Thierry Dang <thierrydang@gmail.com>
2021-12-22 15:48:43 +0900

    feat-set_message_function

--- contracts/DisplayMessage.sol
+++ contracts/DisplayMessage.sol
@@ -3,11 +3,25 @@ pragma solidity >=0.4.22 <0.9.0;
 
 // Contract name
 contract DisplayMessage {
+// Variable that store our message
+    string public _message;
+
+// Function that will set our initial message
+function initMessage() public 
+{
+    _message = "First Smart Contract";
+}
 
 // Function that will display our message
-function myMessage() public pure returns (string memory) 
+function getMessage() public view returns (string memory) 
 {
-    return "First Smart Contract";
+    return _message;
+}
+
+// Function that will set a new message
+function setMessage(string memory newMessage) public
+{
+_message = newMessage;
 }
 
 }
